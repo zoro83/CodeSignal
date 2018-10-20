8b3e161046c54d335e5e6f29a6676001ac041dbe
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-20 17:48:52 +0400

    Test5 Shape Area (Rumbus Area)

---
+++ Test5/Test5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test5
+{
+    class Program
+    {
+        static int ShapeArea(int n)
+        {
+            int sum = 2 * n - 1;
+            for (int i = 2 * n - 3; i > 0; i -= 2)
+            {
+                sum = sum + 2 * i;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            int res = ShapeArea(5);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
