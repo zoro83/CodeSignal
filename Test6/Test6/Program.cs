0eccde2875bf2a57c976be3fea54919d01991a3e
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-20 18:06:09 +0400

    Test6  MakeArrayConsecutive2

---
+++ Test6/Test6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test6
+{
+    class Program
+    {
+        static int MakeArrayConsecutive2(int[] statues)
+        {
+            int count = 0;
+            int min = statues.Min();
+            int max = statues.Max();
+            count = max - min + 1 - statues.Length;
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 4, 9, 6, 1 };
+            int neded = MakeArrayConsecutive2(arr);
+            Console.WriteLine(neded);
+            Console.ReadKey();
+        }
+    }
+}
