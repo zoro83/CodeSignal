29a610fde50b2e9318574a99046132a8d0e196be
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-20 17:20:00 +0400

    Test4 Max product

---
+++ Test4/Test4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4
+{
+    class Program
+    {
+        static int adjacentElementsProduct(int[] inputArray)
+        {
+            int max = inputArray[0] * inputArray[1];
+            for (int i = 0; i < inputArray.Length-1; i++)
+            {
+                if ((int)(inputArray[i] * inputArray[i+1]) > max)
+                {
+                    max = inputArray[i] * inputArray[i + 1];
+                }
+            }
+            return max;
+        }
+            static void Main(string[] args)
+        {
+            int[] res = { 5, 1, 2, 5, 1, 4 };
+            int prod = adjacentElementsProduct(res);
+            Console.WriteLine(prod);
+            Console.ReadKey();
+
+        }
+    }
+}
