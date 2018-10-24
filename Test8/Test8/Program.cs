3643b1baa7b9bf78b14aa04bee6684e5f900f5ae
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-24 20:24:55 +0400

    matrixElementsSum [][]

---
+++ Test8/Test8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test8
+{
+    class Program
+    {
+        static int matrixElementsSum(int[][] matrix)
+        {
+            int coastSum = 0;
+            int row = matrix.Length;
+            int column = matrix[0].Length;
+
+            for (int j = 0; j < column; j++)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        for (int k = i; k < row; k++)
+                        {
+                            matrix[k][j] = 0;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    coastSum += matrix[i][j];
+                }
+            }
+
+            return coastSum;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
