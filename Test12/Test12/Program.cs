06b3d9bdde321918d9967f837119b283ac6d946c
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-25 22:46:11 +0400

    Sort by Height

--- Test12/Test12/Program.cs
+++ Test12/Test12/Program.cs
@@ -8,6 +8,14 @@ namespace Test12
 {
     class Program
     {
+        /*
+         Some people are standing in a row in a park. There are trees between them which cannot be moved. 
+         Your task is to rearrange the people by their heights in a non-descending order without moving the trees. 
+         People can be very tall!
+            Example
+         For a = [-1, 150, 190, 170, -1, -1, 160, 180], the output should be
+         sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
+         */
         static void Swap(int[] arr, int index1, int index2)
         {
             if (index1 != index2)
