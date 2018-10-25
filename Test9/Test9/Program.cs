c436ad18399f3a3b4b0e760d587c8a0cc0705377
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-26 02:15:53 +0400

    All Longest Strings

--- Test9/Test9/Program.cs
+++ Test9/Test9/Program.cs
@@ -8,6 +8,12 @@ namespace Test9
 {
     class Program
     {
+        //Given an array of strings, return another array containing all of its longest strings.
+        //
+        //Example
+        //
+        //For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+        //allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
         string[] allLongestStrings(string[] inputArray)
         {
             int maxLength = inputArray[0].Length;
