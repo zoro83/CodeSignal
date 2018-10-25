5645d45be26f58c3eab8ae40fab63a2337261631
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-26 01:55:10 +0400

    Palindrome Method

--- Test3/Test3/Program.cs
+++ Test3/Test3/Program.cs
@@ -8,6 +8,17 @@ namespace Test3
 {
     class Program
     {
+        //Given the string, check if it is a palindrome.
+
+        //Example
+
+        //For inputString = "aabaa", the output should be
+        //checkPalindrome(inputString) = true;
+        //For inputString = "abac", the output should be
+        //checkPalindrome(inputString) = false;
+        //For inputString = "a", the output should be
+        //checkPalindrome(inputString) = true.
+
         static bool checkPalindrome(string inputString)
         {
             bool flag = true;
