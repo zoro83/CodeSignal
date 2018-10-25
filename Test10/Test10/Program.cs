04189a45c8103ccba3aab28afe90362001675191
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-26 02:17:41 +0400

    Common Character Count (2 array)

--- Test10/Test10/Program.cs
+++ Test10/Test10/Program.cs
@@ -8,6 +8,15 @@ namespace Test10
 {
     class Program
     {
+            // Given two strings, find the number of common characters between them.
+            //
+            //Example
+            //
+            //For s1 = "aabcc" and s2 = "adcaa", the output should be
+            //commonCharacterCount(s1, s2) = 3.
+            //
+            //Strings have 3 common characters - 2 "a"s and 1 "c".
+
         static int commonCharacterCount(string s1, string s2)
         {
             int count = 0;
