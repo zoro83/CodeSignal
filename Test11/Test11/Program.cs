2da6012c14e2a6e02ebd8bac67009e1e83757ca4
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-26 02:19:52 +0400

    Is Lucky (Bilet)

--- Test11/Test11/Program.cs
+++ Test11/Test11/Program.cs
@@ -8,6 +8,18 @@ namespace Test11
 {
     class Program
     {
+        //Ticket numbers usually consist of an even number of digits.
+        //A ticket number is considered lucky if the sum of the first half of the digits is equal to the sum of the 
+        //second half.
+        //
+        //Given a ticket number n, determine if it's lucky or not.
+        //
+        //Example
+        //For n = 1230, the output should be
+        //isLucky(n) = true;
+        //        For n = 239017, the output should be
+        //        isLucky(n) = false.
+
         static bool isLucky(int n)
         {
             int num = n;
