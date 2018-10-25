d11ca06eda923ef30b256405673c44e535e384a3
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-26 02:02:37 +0400

    Shape Area (Rumbus Area)

--- Test5/Test5/Program.cs
+++ Test5/Test5/Program.cs
@@ -8,8 +8,21 @@ namespace Test5
 {
     class Program
     {
+        // Below we will define an n-interesting polygon.Your task is to find the area of a polygon for a given n.
+        //
+        //A 1-interesting polygon is just a square with a side of length 1. 
+        //An n-interesting polygon is obtained by taking the n - 1-interesting polygon and 
+        //appending 1-interesting polygons to its rim, side by side.
+        //You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+        //
+        //Example
+        //
+        //For n = 2, the output should be
+        //shapeArea(n) = 5;
+        //For n = 3, the output should be
+        //shapeArea(n) = 13.
         static int ShapeArea(int n)
-        {
+        { 
             int sum = 2 * n - 1;
             for (int i = 2 * n - 3; i > 0; i -= 2)
             {
