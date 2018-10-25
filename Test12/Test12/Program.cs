88a82d3f32547cd1a44bde1c424cf915837d7fad
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-25 22:43:12 +0400

    Sort by Height

---
+++ Test12/Test12/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test12
+{
+    class Program
+    {
+        static void Swap(int[] arr, int index1, int index2)
+        {
+            if (index1 != index2)
+            {
+                int temp = arr[index1];
+                arr[index1] = arr[index2];
+                arr[index2] = temp;
+
+            }
+        }
+            static int[] sortByHeight(int[] a)
+        {
+            int[] tree = new int[a.Length];
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == -1)
+                {
+                    tree[i] = a[i];
+                    count++;
+                }
+                
+            }
+            int[] human = new int[a.Length - count];
+            for (int i = 0; i < (a.Length - count); )
+            {
+                for (int k = 0; k < a.Length; k++)
+                {
+                    if (a[k] != -1)
+                    {
+                        human[i] = a[k];
+                        Console.WriteLine(human[i]);
+                        i++;
+                    }
+
+                }
+            }
+            for (int i = 0; i < human.Length; i++)
+            {
+                for (int j = 1; j < human.Length-i; j++)
+                {
+                    if (human[j]<human[j-1])
+                    {
+                        Swap(human, j, j - 1);
+                    }
+                }
+            }
+            for (int i = 0; i < human.Length; )
+            {
+                for (int j= 0; j < tree.Length; j++)
+                {
+                    if (tree[j] == 0)
+                    {
+                        tree[j] = human[i];
+                        i++;
+                    }
+                }
+            }
+
+            return tree;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { -1, 150, 190, 170, -1, -1, 160, 180 };
+            a = sortByHeight(a);
+            Console.ReadKey();
+        }
+    }
+}
--- Test12/Test12/Properties/AssemblyInfo.cs
+++ Test12/Test12/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DigitRootSort")]
+[assembly: AssemblyTitle("Test12")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DigitRootSort")]
+[assembly: AssemblyProduct("Test12")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d9386658-c1ef-435e-8751-17ec6e45db81")]
+[assembly: Guid("26d705b1-adbb-46ee-86ae-fe1647fd26dc")]
 
 // Version information for an assembly consists of the following four values:
 //
