47960b064c2004257164d89d374dab3c8cdbaa39
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-26 02:24:36 +0400

    Century From Year

---
+++ Test2/Test2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        //Given a year, return the century it is in. The first century spans from the year 1 up to and 
+        //including the year 100, the second - from the year 101 up to and including the year 200, etc.
+        static int centuryFromYear(int year)
+        {
+            int result = 1 + (int)(year - 1) / 100;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(centuryFromYear(2000));
+            Console.ReadKey();
+        }
+    }
+}
