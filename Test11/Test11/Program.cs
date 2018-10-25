0715d49b8ab5a1bee3405f0cb62ddc826a71d1d9
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-25 21:09:44 +0400

    Is Lucky (Bilet)

---
+++ Test11/Test11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test11
+{
+    class Program
+    {
+        static bool isLucky(int n)
+        {
+            int num = n;
+            int length = Convert.ToString(n).Length;
+            int[] digit = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                digit[i] = num % 10;
+                num /= 10;
+               // Console.WriteLine(digit[i]);
+            }
+            int sumFirst = 0;
+            int sumLast = 0;
+            for (int i = 0; i < length / 2; i++)
+            {
+                sumLast += digit[i];
+            }
+            for (int i = length / 2; i < length; i++)
+            {
+                sumFirst += digit[i];
+            }
+            if (sumFirst == sumLast)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 1230;
+            Console.WriteLine(isLucky(n));
+            Console.ReadKey();
+        }
+    }
+}
