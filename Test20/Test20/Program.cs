4f49ae4ba4de142e3870f3abc08d843ff5f39b0d
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-10-28 00:34:44 +0400

    Array Maximal Adjacent Difference!

---
+++ Test20/Test20/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test20
+{
+    class Program
+    {
+        //Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+        //
+        //Example
+        //
+        //For inputArray = [2, 4, 1, 0], the output should be
+        //arrayMaximalAdjacentDifference(inputArray) = 3.
+
+        static int arrayMaximalAdjacentDifference(int[] inputArray)
+        {
+            int difference = 0;
+
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                if (Math.Abs(inputArray[i] - inputArray[i + 1]) > difference)
+                {
+                    difference = Math.Abs(inputArray[i] - inputArray[i + 1]);
+                }
+            }
+            return difference;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
