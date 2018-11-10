014bca0795dc1765d755b319e6cdb81ed2755077
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-11 00:23:46 +0400

    Array Replace

--- Test25/Test25/Program.cs
+++ Test25/Test25/Program.cs
@@ -8,6 +8,12 @@ namespace Test25
 {
     class Program
     {
+        // Given an array of integers, replace all the occurrences of elemToReplace with 
+        // substitutionElem.
+        // Example
+        // For inputArray = [1, 2, 1], elemToReplace = 1, and substitutionElem = 3, the output should be
+        // arrayReplace(inputArray, elemToReplace, substitutionElem) = [3, 2, 3].
+
         static int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem)
         {
             for (int i = 0; i < inputArray.Length; i++)
