3e8efb32712622cd59e902e9f7ce49205b966815
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-11 00:35:31 +0400

    Even DigitsOnly

---
+++ Test26/Test26/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test26
+{
+    class Program
+    {
+        bool evenDigitsOnly(int n)
+        {
+            int num = n;
+            bool flag = true;
+            for (int i = 0; i < Convert.ToString(n).Length; i++)
+            {
+                if (num % 10 % 2 == 0)
+                {
+                    num /= 10;
+                }
+                else
+                    flag = false;
+            }
+
+            return flag;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
