614600037c0f86f4c2267aa60b4a52e80a1d46b4
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-11 00:21:45 +0400

    Array Replace

---
+++ Test25/Test25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test25
+{
+    class Program
+    {
+        static int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem)
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] == elemToReplace)
+                {
+                    inputArray[i] = substitutionElem;
+                }
+            }
+            return inputArray;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 1 };
+            int[] replace = arrayReplace(arr, 1, 3);
+            for (int i = 0; i < replace.Length; i++)
+            {
+                Console.WriteLine(replace[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
