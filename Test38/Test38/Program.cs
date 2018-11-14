af904ff274f07880fb192fa35522c6aa797476c6
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-14 21:58:33 +0400

    Growing Plant

--- Test38/Test38/Program.cs
+++ Test38/Test38/Program.cs
@@ -8,6 +8,14 @@ namespace Test38
 {
     class Program
     {
+        //Each day a plant is growing by upSpeed meters.Each night that plant's height decreases by 
+        //downSpeed meters due to the lack of sun heat. Initially, plant is 0 meters tall. 
+        //We plant the seed at the beginning of a day. We want to know when the height of the plant will 
+        //reach a certain level.
+        //Example
+        //For upSpeed = 100, downSpeed = 10, and desiredHeight = 910, the output should be
+        //growingPlant(upSpeed, downSpeed, desiredHeight) = 10.
+
         public static int growingPlant(int upSpeed, int downSpeed, int desiredHeight)
         {
             int days = 1;
