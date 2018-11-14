4617f21b959f1b768ca180f0872255dfa2b9e9e0
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-14 21:48:28 +0400

    Growing Plant

---
+++ Test38/Test38/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test38
+{
+    class Program
+    {
+        public static int growingPlant(int upSpeed, int downSpeed, int desiredHeight)
+        {
+            int days = 1;
+            int height = 0;
+            while (true)
+            {
+                height += upSpeed;
+                if (height >= desiredHeight)
+                {
+                    break;
+                }
+                height -= downSpeed;
+                days++;
+            }
+            return days;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(growingPlant(100, 10, 910));
+            Console.ReadKey();
+        }
+    }
+}
