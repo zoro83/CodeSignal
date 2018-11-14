6d7982644d34f9d8c6e75c1de472a46a165c0b09
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-14 17:56:48 +0400

    First Digit

---
+++ Test35/Test35/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test35
+{
+    class Program
+    {
+    //Find the leftmost digit that occurs in a given string.
+    //Example
+    //For inputString = "var_1__Int", the output should be
+    //firstDigit(inputString) = '1';
+    //For inputString = "q2q-q", the output should be
+    //firstDigit(inputString) = '2';
+    //For inputString = "0ss", the output should be
+    //firstDigit(inputString) = '0'.
+
+        public static char firstDigit(string inputString)
+        {
+            char num = ' ';
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (char.IsNumber(inputString[i]))
+                {
+                    num = inputString[i];
+                    break;
+                }
+
+            }
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
