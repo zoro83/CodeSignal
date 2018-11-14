096cb2c6e509dee957d9955c965e491690383f7d
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-14 19:32:05 +0400

    Strings Rearrangement

--- Test33/Test33/Program.cs
+++ Test33/Test33/Program.cs
@@ -8,34 +8,24 @@ namespace Test33
 {
     class Program
     {
-        public static int diferentByOneCharacter(string str1, string str2)
-        {
-            int count = 0;
-            for (int i = 0; i < str1.Length; i++)
-            {
-                if (str1[i] != str2[i])
-                {
-                    count++;
-                }
-            }
-            return count;
-        }
-        public static bool stringsRearrangement(string[] inputArray)
-        {
-            bool res = false;
-            int count = 0;
-            for (int i = 0; i < inputArray.Length - 1; i++)
-            {
-                for (int j = i + 1; j < inputArray.Length; j++)
-                {
-                    if (diferentByOneCharacter(inputArray[i], inputArray[j]) == 1)
-                    {
-                        count++;
-                    }
-                }
-            }
+        //Given an array of equal-length strings, check if it is possible to rearrange the strings in such a way 
+        //that after the rearrangement the strings at consecutive positions would differ by exactly one character.
+        //Example
+        //For inputArray = ["aba", "bbb", "bab"], the output should be
+        //stringsRearrangement(inputArray) = false.
+        //All rearrangements don't satisfy the description condition.
+        //For inputArray = ["ab", "bb", "aa"], the output should be
+        //stringsRearrangement(inputArray) = true.
+        //Strings can be rearranged in the following way: "aa", "ab", "bb"
 
-            return (count > inputArray.Length - 1) ? true : false;
+        bool stringsRearrangement(string[] r)
+        {
+            return r.Select((x, i) => i + x).Any(a => IsSolved(r.Select((x, i) => i + x), a));
+        }
+
+        bool IsSolved(IEnumerable<string> rs, string txt)
+        {
+            return rs.Count() < 2 || rs.Where(x => x.Where((c, i) => i > 0 & c != txt[i]).Count() == 1).Any(x => IsSolved(rs.Except(new[] { txt }), x));
         }
         static void Main(string[] args)
         {
