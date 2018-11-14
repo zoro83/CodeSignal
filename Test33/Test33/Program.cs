9b83fda8b71ad1a37e98b2fe18b6f7d0594a5540
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-14 17:38:33 +0400

    Extract Each Kth

---
+++ Test33/Test33/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test33
+{
+    class Program
+    {
+        public static int diferentByOneCharacter(string str1, string str2)
+        {
+            int count = 0;
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] != str2[i])
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static bool stringsRearrangement(string[] inputArray)
+        {
+            bool res = false;
+            int count = 0;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    if (diferentByOneCharacter(inputArray[i], inputArray[j]) == 1)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return (count > inputArray.Length - 1) ? true : false;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Test34/Test34/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test34
+{
+    class Program
+    {
+        //Given array of integers, remove each kth element from it.
+        //Example
+        //For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3, the output should be
+        //extractEachKth(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].
+
+        public static int[] extractEachKth(int[] inputArray, int k)
+        {
+            int[] outputArray = new int[inputArray.Length - inputArray.Length / k];
+            int count = 1;
+            for (int i = 0; i < outputArray.Length; i++)
+            {
+                if (i + count - 1 == count * k - 1)
+                {
+                    count++;
+                }
+                outputArray[i] = inputArray[i + count - 1];
+            }
+            return outputArray;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] test = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] res = extractEachKth(test, 3);
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.Write($"{res[i]}, ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
