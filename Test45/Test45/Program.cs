09d6ff7fbbb996dcd2451b1d130247746d16b9ec
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2018-11-20 01:14:53 +0400

    Build Palindrome

---
+++ Test45/Test45/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test45
+{
+    class Program
+    {
+
+        public static string buildPalindrome(string st)
+        {
+            string polidrome = st;
+            int count = 0;
+
+            for (int i = 0; i < polidrome.Length / 2;)
+            {
+                if (polidrome[i] == polidrome[polidrome.Length - 1 - i])
+                {
+                    i++;
+                    count++;
+                }
+                else
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        polidrome = polidrome.Insert(st.Length + j, $"{st[i - j]}");
+                    }
+                    //Console.WriteLine(polidrome);
+                    count = 1;
+                }
+            }
+            return polidrome;
+        }
+        static void Main(string[] args)
+        {
+            string st = "abcdckba";
+            //st = st.Insert(4, "n");
+            //Console.WriteLine(st);
+            Console.WriteLine(buildPalindrome(st));
+            Console.ReadKey();
+        }
+    }
+}
