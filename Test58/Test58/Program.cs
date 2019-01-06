4075516ac8bfb71e9c87fe8cca8ffb03421bc7cb
Zohrab Karapetyan <zohrabkarapetyan1983@gmail.com>
2019-01-07 02:05:27 +0400

    Message From Binary Code (Binary translation)

---
+++ Test58/Test58/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test58
+{
+    //You are taking part in an Escape Room challenge designed specifically for programmers.
+    //In your efforts to find a clue, you've found a binary code written on the wall behind a vase, 
+    //and realized that it must be an encrypted message. After some thought, your first guess is that 
+    //each consecutive 8 bits of the code stand for the character with the corresponding extended ASCII code.
+    //Assuming that your hunch is correct, decode the message.
+    //Example
+    //For code = "010010000110010101101100011011000110111100100001", the output should be
+    //messageFromBinaryCode(code) = "Hello!".
+
+    class Program
+    {
+        public static int BitStringToInt(string bits)
+        {
+            int num = 0;
+            for (int i = bits.Length - 1; i >= 0; i--)
+            {
+                if (bits[i] == '1')
+                {
+                    num += (int)Math.Pow(2, bits.Length - 1 - i);
+                }
+            }
+            return num;
+        }
+        public static string messageFromBinaryCode(string code)
+        {
+            char key;
+            string msg = "";
+            for (int i = 0; i < code.Length; i += 8)
+            {
+                key = Convert.ToChar(BitStringToInt(code.Substring(i, 8)));
+                msg += key;
+            }
+            return msg;
+        }
+
+        static void Main(string[] args)
+        {
+            string str = "010010000110010101101100011011000110111100100001";
+            //int num = BitStringToInt(str);
+            //Console.WriteLine(num);
+            //char ch = 'Q';
+            //Console.WriteLine(Convert.ToInt32(ch));
+            Console.WriteLine(messageFromBinaryCode(str));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test58/Test58/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Test58")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test58")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f28f7c07-7769-4a60-8721-8ed21253406b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
